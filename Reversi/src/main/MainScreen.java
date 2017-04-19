252750959a128168d6ed9eddccd677da77a096c5
Morgan <quixoticbibliomania@gmail.com>
2017-04-19 11:15:24 -0400

    player choice, etc

--- Reversi/src/main/MainScreen.java
+++ Reversi/src/main/MainScreen.java
@@ -1,10 +1,15 @@
 package main;
 
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
+import ai.AI;
+import ai.GeneralAI;
 import util.ImageGrab;
 
 public class MainScreen extends JFrame {
@@ -12,11 +17,21 @@ public class MainScreen extends JFrame {
 	private static Piece[][] board;
 	private static PieceLabel[][] visualBoard;
 	private static int player;
+	private static AI comp;
 	public static void main(String[] args) {
 		new MainScreen();
 	}
 	public MainScreen() {
-		//set player to what they choose
+		comp = new GeneralAI();
+		Object[] colors = {"Black" , "White"};
+		int answer = JOptionPane.showOptionDialog(this, "Would you like to play as black or white?",
+				"Select Player",
+				JOptionPane.DEFAULT_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				colors,
+				colors[0]);
+		player = answer == 0?Piece.BLACK:Piece.WHITE;
 		setLayout(new GridLayout(8,8));
 		new ImageGrab();
 		board = new Piece[8][8];
@@ -28,13 +43,11 @@ public class MainScreen extends JFrame {
 		}
 		for (int i=0;i<visualBoard[0].length;i++) {
 			for (int j=0;j<visualBoard.length;j++) {
-				visualBoard[i][j] = new PieceLabel(Piece.BLANK);
+				updateBoard(i,j,Piece.BLANK);
 				if (i==3 && j==3 || i==4 && j==4) {
-					visualBoard[i][j] = new PieceLabel(Piece.WHITE);
-					board[i][j] = new Piece(Piece.WHITE);
+					updateBoard(i,j,Piece.WHITE);
 				} else if (i==4 && j == 3 || i==3 && j==4) {
-					visualBoard[i][j] = new PieceLabel(Piece.BLACK);
-					board[i][j] = new Piece(Piece.BLACK);
+					updateBoard(i,j,Piece.BLACK);
 				}
 				add(visualBoard[i][j]);
 			}
@@ -42,16 +55,56 @@ public class MainScreen extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
+		do {
+			//TODO turncycle
+		} while (!gameOver());
+	}
+	private static boolean gameOver() {
+		// TODO
+		return false;
+	}
+	/**
+	 * Updates the board & the visualBoard
+	 * @param x the x-coordinate
+	 * @param y the y-coordinate
+	 * @param piece the piece to change it to
+	 * @return true if the update succeeded, false otherwise
+	 */
+	private static boolean updateBoard(int x, int y, int piece) {
+		try {
+			if (visualBoard[x][y]==null) {
+				visualBoard[x][y] = new PieceLabel(x,y,piece);
+			} else {
+				visualBoard[x][y].setPiece(piece);
+			}
+			board[x][y] = new Piece(piece);
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
 	}
 	private static class PieceLabel extends JLabel {
 		private static final long serialVersionUID = 5423659178406010534L;
-		private int piece;
-		public PieceLabel(int piece) {
+		private int piece,x,y;
+		public PieceLabel(int x, int y, int piece) {
 			super(ImageGrab.getIconFromPiece(piece));
 			this.piece=piece;
+			this.x=x;
+			this.y=y;
+			addMouseListener(new MouseAdapter() {
+				public void mouseReleased(MouseEvent e) {
+					//TODO
+					System.out.println(x + " , " + y);
+					updateBoard(x,y,player);
+				}
+			});
 		}
 		public int getPiece() {
 			return piece;
+		}
+		public void setPiece(int piece) {
+			this.piece=piece;
+			setIcon(ImageGrab.getIconFromPiece(piece));
 		}
 	}
 }
